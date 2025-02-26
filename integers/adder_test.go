6f6c364bc67324d13d8a0cd9140493901b2645fc
Immanuella <emi.busari@gmail.com>
2025-02-26 00:45:08 +0000

    Integers - final changes

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,16 +1,30 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	t.Run("add two numbers", func(t *testing.T) {
-		sum := Add(3, 2);
-		expected := 5;
+		sum := Add(3, 2)
+		expected := 5
 
-		if sum != expected {
-			// We use %d instead of %q because we want to print an integer rather than strings
-			t.Errorf("Expected '%d' but got '%d'", expected, sum);
-		}
-
+		assertCorrectMessage(t, sum, expected)
 	})
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+func assertCorrectMessage(t testing.TB, got, want int) {
+	t.Helper()
+
+	if got != want {
+		// We use %d instead of %q because we want to print an integer rather than strings
+		t.Errorf("Expected '%d' but got '%d'", want, got)
+	}
 }
